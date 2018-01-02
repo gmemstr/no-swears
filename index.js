584e6e51b3d0020bfa9e463ac746ec6ee070f76f
Alan Russell <ajrussellaudio@gmail.com>
2018-01-02 22:15:33 +0000

    change output so that number of *s matches length of swear

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * no-swears - Filter out or detect swear words in your strings
  *
  * @author Gabriel Simmer <https://github.com/gmemstr>
@@ -6,45 +6,45 @@
  * @license MIT
  **/
 
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-	/**
+  /**
    * Replaces bad words with asterisks
    **/
-	filter: (words, callback) => {
-		var finalString = words
-		swearList(lines => {
-			for (var i = 0; i < lines.length; i++) {
-				var bw = new RegExp(lines[i], 'gi')
-				finalString = finalString.replace(bw, '****')
-			}
-			callback(finalString)
-		});
-	},
-	/**
+  filter: (words, callback) => {
+    var finalString = words;
+    swearList(lines => {
+      for (var i = 0; i < lines.length; i++) {
+        var bw = new RegExp(lines[i], "gi");
+        finalString = finalString.replace(bw, "*".repeat(lines[i].length));
+      }
+      callback(finalString);
+    });
+  },
+  /**
    * True if swearword is found, false if not
    **/
-	hasSwears: (words, callback) => {
-		swearList(lines =>{
-			var b = false
-			for (var i = 0; i < lines.length; i++) {
-				if(words.indexOf(lines[i]) > -1) {
-					b = true
-				}
-			}
-			callback(b)
-		});
-	}
-}
+  hasSwears: (words, callback) => {
+    swearList(lines => {
+      var b = false;
+      for (var i = 0; i < lines.length; i++) {
+        if (words.indexOf(lines[i]) > -1) {
+          b = true;
+        }
+      }
+      callback(b);
+    });
+  }
+};
 
 var swearList = callback => {
-	fs.readFile(path.join(__dirname,'swearwords.txt'), 'utf8', (err, data) => {
-		if (err) throw err
-		data = data.replace(new RegExp('\r','g'),'').split('\n')
-		callback(data)
-	});
-}
+  fs.readFile(path.join(__dirname, "swearwords.txt"), "utf8", (err, data) => {
+    if (err) throw err;
+    data = data.replace(new RegExp("\r", "g"), "").split("\n");
+    callback(data);
+  });
+};
